780683ecf53e371e190bc3957f4388c382fc0369
Htgar <development@htgar.org>
2024-03-09 09:42:48 +0800

    Fixed statusline not appearing

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -7,6 +7,7 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
+    'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup {
